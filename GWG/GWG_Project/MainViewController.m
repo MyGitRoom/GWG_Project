65975a1f1a13770c518cfde9f1891e879421c636
ZF <lan@lanoudeMac-mini-174.local>
2016-05-06 19:32:51 +0800

    zf

--- GWG/GWG_Project/MainViewController.m
+++ GWG/GWG_Project/MainViewController.m
@@ -14,6 +14,7 @@
 #import "RadioViewController.h"
 #import "MovieViewController.h"
 #import "UIImageView+WebCache.h"
+#import "LazyFadeInView.h"
 #define DAILYURL @"http://dict-mobile.iciba.com/interface/index.php?c=sentence&m=getsentence&client=1&type=1&field=1,2,3,4,5,6,7,8,9,10,11,12,13&timestamp=1434767570&sign=6124a62ff73a033a&uuid=3dd23ff24ea543c1bdca57073d0540e1&uid="
 @interface MainViewController ()
 
@@ -27,10 +28,14 @@
 @property (nonatomic, strong) NSMutableArray * msgArray;
 @property (nonatomic, strong) NSString * tagMP3; //音频的URL
 @property (nonatomic, strong) UIButton * mainBtn;
-
+@property (nonatomic ,strong) UIButton *hideBtn ;//隐藏主页中间View的按钮
+@property (nonatomic ,strong) UIButton *showViewBtn ;//展开主语中间的Vie的按钮
 
 @property (nonatomic ,strong) NSTimer *Scaletimer ;//创建一个定时器控制按钮动画
+@property (nonatomic ,strong) UIVisualEffectView *effectvi ;//创建毛玻璃的View
 
+@property (nonatomic ,strong) LazyFadeInView *wordView ;
+@property (nonatomic ,strong) NSString *word ;
 @end
 
 @implementation MainViewController
@@ -56,9 +61,21 @@
 
 
 #pragma mark- 隐藏导航栏
+
 -(void)viewWillAppear:(BOOL)animated {
  self.navigationController.navigationBarHidden = YES ;
     
+//    [UIView animateWithDuration:1 animations:^{
+//        self.wordView.layer.transform = CATransform3DMakeScale(1, 1, 1);
+//    }];
+    
+    
+//    [self.view addSubview:self.wordView];
+    NSLog(@"%f",self.wordView.frame.size.width);
+//    self.wordView.hidden = NO ;
+    self.wordView = [[LazyFadeInView alloc]initWithFrame:CGRectMake(20, 60, 100, 300)];
+    self.wordView.text = self.word ;
+    [self.view addSubview:self.wordView];
 }
 
 
@@ -72,17 +89,34 @@
     self.imagev.image = [UIImage imageNamed:@"1.jpg"];
     [NSTimer scheduledTimerWithTimeInterval:4 target:self selector:@selector(changePic) userInfo:nil repeats:YES];
     
+    
+    //创建展开中间View 的按钮
+    self.showViewBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    self.showViewBtn.frame = CGRectMake(KScreenWidth-30, KScreenHeight-30, 60, 60);
+    self.showViewBtn.layer.cornerRadius = 30 ;
+    self.showViewBtn.layer.masksToBounds = YES ;
+    [self.showViewBtn addTarget:self action:@selector(showView:) forControlEvents:UIControlEventTouchUpInside];
+    [self.showViewBtn setImage:[UIImage imageNamed:@"showView"] forState:UIControlStateNormal];
+    self.showViewBtn.backgroundColor = [UIColor colorWithRed:0.778 green:0.894 blue:1.000 alpha:0.700];
+    self.showViewBtn.hidden = YES ;
+    
     //添加毛玻璃效果
     
     UIBlurEffect *blur = [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];
-    UIVisualEffectView *effectvi = [[UIVisualEffectView alloc]initWithEffect:blur];
-    effectvi.frame = CGRectMake(0, 0,KScreenWidth  , KScreenHeight);
-    effectvi.alpha = .95;
+    self.effectvi = [[UIVisualEffectView alloc]initWithEffect:blur];
+    self.effectvi.frame = CGRectMake(0, 0,KScreenWidth  , KScreenHeight);
+    self.effectvi.alpha = .95;
     [self.view addSubview:self.imagev];
-    [self.view addSubview:effectvi];
+    [self.view addSubview:self.effectvi];
+   
+    [self.view addSubview:self.showViewBtn];
     
-//    effectvi.hidden = YES ;
-//    self.vi.hidden = YES ;
+
+    
+    //创建显示渐变文字的View
+    self.wordView = [[LazyFadeInView alloc]initWithFrame:CGRectMake(20, 60, 100, 300)];
+
+     [self.view addSubview:self.wordView];
     
     //创建标签云
 
@@ -91,6 +125,7 @@
     //加载数据
     [self requestData];
     
+    self.wordView.text = self.word ;
 }
 
 
@@ -167,10 +202,6 @@
     
     if (_i==5) {
 
-//    self.imagev.image =[UIImage imageNamed:[NSString stringWithFormat:@"%ld.jpg",_i]];
-    
-//    if (_i==4) {
-
         _i=1;
     }
     self.imagev.image =[UIImage imageNamed:[NSString stringWithFormat:@"%ld.jpg",_i]];
@@ -205,7 +236,11 @@
         msg.translation = [dataDic objectForKey:@"translation"];
         msg.tts = [dataDic objectForKey:@"tts"];
         [self.msgArray addObject:msg];
-        //        NSLog(@"%@",[_msgArray objectAtIndex:0]);
+         //        NSLog(@"%@",[_msgArray objectAtIndex:0]);
+        
+        //赋值给word
+       
+        self.word = msg.content ;
         
         dispatch_async(dispatch_get_main_queue(), ^{
             [self loadTodayView];
@@ -229,11 +264,11 @@
 
     _vi.layer.cornerRadius = 7;
     _vi.layer.masksToBounds = YES;
-    [self.view addSubview:_vi];
+    [self.effectvi addSubview:_vi];
     [self createControls];
 }
 
-//创建视图上控件
+#pragma mark -- 创建视图上控件
 - (void) createControls
 {
     
@@ -243,8 +278,19 @@
     //日期Label
     UILabel * dateLabel = [[UILabel alloc]initWithFrame:CGRectMake(10, 10, self.vi.frame.size.width, 40)];
     dateLabel.text = msg.title;
-    [self.vi addSubview:dateLabel];
     
+    //隐藏中间View的按钮
+    self.hideBtn = [UIButton buttonWithType:UIButtonTypeCustom] ;
+    self.hideBtn.frame = CGRectMake(self.vi.frame.size.width-35, 15, 25, 25);
+    [self.hideBtn setBackgroundImage:[UIImage imageNamed:@"close"] forState:UIControlStateNormal];
+//    self.hideBtn.backgroundColor = [UIColor blueColor];
+    [self.hideBtn addTarget:self action:@selector(hideView:) forControlEvents:UIControlEventTouchUpInside ];
+    self.hideBtn.layer.cornerRadius = 25/2;
+    self.hideBtn.layer.masksToBounds = YES ;
+    
+    
+    [self.vi addSubview:dateLabel];
+    [self.vi addSubview:self.hideBtn];
     //图片
     self.imageV = [[UIImageView alloc]initWithFrame:CGRectMake(0, 40+20, self.vi.frame.size.width, self.vi.frame.size.width*0.618)];
 
@@ -279,9 +325,93 @@
     _mainBtn.backgroundColor = [UIColor colorWithRed:60/255.5 green:74/255.0 blue:157/255.0 alpha:.8] ;
 //    _mainBtn.backgroundColor = [UIColor colorWithRed:0.996 green:0.824 blue:0.224 alpha:1.000];
     
+    
+    
 
 }
 
+#pragma  mark - 实现隐藏中间View 的按钮 
+
+-(void)hideView:(UIButton *)hideBtn{
+    
+    [UIView animateWithDuration:3 animations:^{
+
+        CAKeyframeAnimation *keyAnimation = [CAKeyframeAnimation animationWithKeyPath:@"position"];
+        CGMutablePathRef path = CGPathCreateMutable() ;
+        //指定self.vi 的中心为起点
+//        CGPathMoveToPoint(path, NULL,self.vi.frame.origin.x, self.vi.frame.origin.y);
+        CGPathMoveToPoint(path, NULL,self.vi.center.x, self.vi.center.y);
+//        CGPathMoveToPoint(path, NULL, KScreenWidth/2-KScreenWidth/1.3/2, KScreenHeight/2-KScreenHeight/1.3/2-40);
+        CGPathAddLineToPoint(path, NULL, KScreenWidth,KScreenHeight);
+        //设置时间
+        [keyAnimation setDuration:2];
+        
+        //设置path
+        [keyAnimation setPath:path];
+        
+          //设置动画执行完毕后，不删除动画
+        keyAnimation.removedOnCompletion=NO;
+            //设置保存动画的最新状态
+        keyAnimation.fillMode=kCAFillModeForwards;
+        
+        self.vi.layer.transform = CATransform3DMakeScale(0.00000001,0.00000001, 1);
+        [self.vi.layer addAnimation:keyAnimation forKey:@"position"];
+        
+    } completion:^(BOOL finished) {
+        self.wordView.hidden = NO ;
+        self.showViewBtn.hidden = NO ;
+        
+        self.wordView.text = self.word ;
+        [UIView animateWithDuration:0.8 animations:^{
+            self.showViewBtn.layer.transform = CATransform3DMakeScale(1.5, 1.5, 1);
+
+        } completion:^(BOOL finished) {
+            [UIView animateWithDuration:1 animations:^{
+                 self.showViewBtn.layer.transform = CATransform3DMakeScale(0.9, 0.9, 1);
+            }];
+           
+
+        }];
+        
+    }];
+
+}
+
+#pragma  mark - 显示隐藏的中间View
+-(void)showView:(UIButton *)showViewBtn{
+    
+    self.wordView.hidden = YES ;
+    [UIView animateWithDuration:2 animations:^{
+        CAKeyframeAnimation *keyAnimation = [CAKeyframeAnimation animationWithKeyPath:@"position"];
+        CGMutablePathRef path = CGPathCreateMutable() ;
+        //指定self.vi 的中心为起点
+        CGPathMoveToPoint(path, NULL, KScreenWidth,KScreenHeight);
+        //设置终点
+        CGPathAddLineToPoint(path, NULL,self.vi.frame.origin.x, self.vi.frame.origin.y);
+        //设置时间
+        [keyAnimation setDuration:2];
+        
+        //设置path
+        [keyAnimation setPath:path];
+        
+        //设置动画执行完毕后，不删除动画
+        keyAnimation.removedOnCompletion=NO;
+        //设置保存动画的最新状态
+        keyAnimation.fillMode=kCAFillModeForwards;
+        
+        [self.vi.layer addAnimation:keyAnimation forKey:@"position"];
+        
+        self.effectvi.layer.transform = CATransform3DMakeScale(1, 1, 1) ;
+        self.vi.layer.transform = CATransform3DMakeScale(1, 1, 1);
+        self.showViewBtn.layer.transform = CATransform3DMakeScale(0.0001, 0.0001, 0.0001);
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:2 animations:^{
+            self.showViewBtn.layer.transform = CATransform3DMakeScale(0, 0, 0);
+        }];
+    }];
+
+
+}
 
 
 - (void) touchChange:(UIButton *)btn
